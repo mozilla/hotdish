00031c34915409b4e195e1dab4000356ee8f67f0
Ian Bicking <ianb@colorstudy.com>
2014-02-25 01:21:22 -0600

    Fix #312.  Fix #292.  Add group names, so that the group ID is more hidden.  Add an interface to the camera page to get the invite link, set the group name, and end the session.  The camera page also has controls to start Hotdish if you open it when Hotdish is not already activated.  The Hotdish button now opens the camera page.

--- data/interaction-cam/pagemod.js
+++ data/interaction-cam/pagemod.js
@@ -16,11 +16,14 @@ window.addEventListener("set-avatar",function(evt) {
   self.port.emit('set-avatar',evt.detail.imgdata);
 });
 
+/************************************************************
+ Name input
+*/
+
 var nameInput = document.querySelector("#name-input");
 self.port.on("set-name", function (name) {
   nameInput.value = name;
 });
-
 
 function updateName() {
   if (nameInput.value) {
@@ -36,5 +39,98 @@ function updateNameSoon() {
 
 nameInput.addEventListener("change", updateName, false);
 nameInput.addEventListener("keyup", updateNameSoon, false);
+
+/************************************************************
+ Group Name input
+*/
+
+var groupNameInput = document.querySelector("#group-name");
+
+function updateGroupName() {
+  if (groupNameInput.value) {
+    self.port.emit("set-group-name", groupNameInput.value);
+  }
+}
+
+var groupUpdater;
+function updateGroupNameSoon() {
+  clearTimeout(groupUpdater);
+  groupUpdater = setTimeout(updateGroupName, 500);
+}
+
+groupNameInput.addEventListener("change", updateGroupName, false);
+groupNameInput.addEventListener("keyup", updateGroupNameSoon, false);
+
+/************************************************************
+ This group and group list
+*/
+
+var thisGroup = document.querySelector("#this-group");
+var groupList = document.querySelector("#group-list");
+var openInCurrent = document.querySelector("#open-in-current");
+var inviteLink = document.querySelector("#invite-link");
+var inviteLinkUrl = null;
+
+self.port.on("thisGroup", function (groupData) {
+  if (! groupData) {
+    thisGroup.style.display = "none";
+    groupList.style.display = "";
+    inviteLinkUrl = inviteLink.value = "";
+    document.body.classList.add("no-group");
+    document.body.classList.remove("in-group");
+  } else {
+    thisGroup.style.display = "";
+    groupList.style.display = "none";
+    inviteLinkUrl = inviteLink.value = groupData.inviteLink;
+    groupNameInput.value = groupData.name;
+    document.body.classList.remove("no-group");
+    document.body.classList.add("in-group");
+  }
+});
+
+document.querySelector("#copy-invite-link").addEventListener("click", function () {
+  self.port.emit("copy-invite-link");
+});
+
+inviteLink.addEventListener("change", function () {
+  if (inviteLinkUrl) {
+    inviteLink.value = inviteLinkUrl;
+  }
+});
+
+inviteLink.addEventListener("focus", function () {
+  inviteLink.select();
+});
+
+document.querySelector("#end-session").addEventListener("click", function () {
+  self.port.emit("end-session");
+});
+
+self.port.on("groupList", function (list) {
+  var tmpl = document.querySelector("#group-template");
+  var container = document.querySelector("#group-list-items");
+  container.innerHTML = "";
+  list.forEach(function (groupData) {
+    var el = tmpl.cloneNode(true);
+    el.id = "";
+    var button = el.querySelector("button.start");
+    button.setAttribute("data-group", JSON.stringify(groupData));
+    button.textContent = groupData.name || groupData.groupId;
+    button.addEventListener("click", function () {
+      self.port.emit("start-new-group", groupData, openInCurrent.checked);
+    });
+    var remove = el.querySelector("button.remove");
+    remove.addEventListener("click", function () {
+      if (window.confirm("Are you sure you want to forget this group?")) {
+        self.port.emit("remove-group", groupData.groupId);
+      }
+    });
+    container.appendChild(el);
+  });
+});
+
+document.querySelector("#start-new-group").addEventListener("click", function () {
+  self.port.emit("start-new-group", null, openInCurrent.checked);
+});
 
 self.port.emit("ready");
